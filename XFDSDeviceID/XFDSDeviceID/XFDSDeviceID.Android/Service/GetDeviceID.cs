d38df41ec635c281422d6ab47ac2bb8c67022e29
weilianlai <weilian@gmail.com>
2017-11-23 11:04:18 +0800

    Xamarin 作業 利用相依性注入方式取得裝置的設備ID XFDSDeviceID

---
+++ XFDSDeviceID/XFDSDeviceID/XFDSDeviceID.Android/Service/GetDeviceID.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XFDSDeviceID.Interfaces;
+using XFDSDeviceID.Droid;
+
+[assembly: Xamarin.Forms.Dependency(typeof(GetDeviceID))]
+
+namespace XFDSDeviceID.Droid
+{
+    public class GetDeviceID : IGetDeviceID
+    {
+        string IGetDeviceID.GetDeviceID()
+        {
+            return Android.OS.Build.Serial;
+        }
+    }
+}
---
+++ XFDSDeviceID/XFDSDeviceID/XFDSDeviceID/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using XFDSDeviceID.Interfaces;
+
+namespace XFDSDeviceID.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly INavigationService _navigationService;
+        public string DeviceID { get; set; }
+        public string DispalyID { get; set; }
+        public string Title { get; set; }
+
+        IGetDeviceID _getDeviceID;
+
+        public MainPageViewModel(INavigationService navigationService, IGetDeviceID getDeviceID)
+        {
+            _navigationService = navigationService;
+            _getDeviceID = getDeviceID;
+            DispalyID = _getDeviceID.GetDeviceID();
+
+            Title = "取得手機裝置唯一ID";
+            DeviceID = "此裝置唯一的設備ID是：";
+            
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+
+}
