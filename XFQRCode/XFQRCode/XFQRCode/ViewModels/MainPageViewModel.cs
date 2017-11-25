37262a4a464afb82e4d8f3a3b1769a1fd3035bf0
A <B>
2017-11-25 16:29:00 +0800

    加入 XFQRCode 專案

---
+++ XFQRCode/XFQRCode/XFQRCode/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFQRCode.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string BarcodeFormatType { get; set; }
+        public string BarcodeResult { get; set; }
+
+        public DelegateCommand ScanCommand { get; set; }
+
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            ScanCommand = new DelegateCommand(() =>
+            {
+                _navigationService.NavigateAsync("");
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+
+}
