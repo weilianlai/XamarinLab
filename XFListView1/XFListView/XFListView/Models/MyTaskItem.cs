00e41c3d559a413d50357b0459752ea17dc9c350
A <B>
2017-11-26 15:14:25 +0800

    XFListView 0 加上下拉更新功能

---
+++ XFListView1/XFListView/XFListView/Models/MyTaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFListView.Models
+{
+    public class MyTaskItem 
+    {
+        //工作名稱
+        public string MyTaskName { get; set; }
+        //狀態
+        public string MyTaskStatus { get; set; }
+        //指派日期
+        public DateTime MyTaskDate { get; set; }
+    }
+}
